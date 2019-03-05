73898d165e27a10e884a8141bf33ed537e0e4bde
Dmitry Malinin <pdacity@ya.ru>
2019-03-05 15:28:45 +0300

    fix mongodb version

--- docker-install.sh
+++ docker-install.sh
@@ -1,7 +1,7 @@
 set -ex
 
 if [ "${MONGODB_VERSION}" != "no" ]; then
-    echo 'deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse' > /etc/apt/sources.list.d/mongodb-org-3.2.list
+    echo 'deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.7 multiverse' > /etc/apt/sources.list.d/mongodb-org-3.7.list
     apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 42F3E95A2C4F08279C4960ADD68FA50FEA312927
 fi
 
